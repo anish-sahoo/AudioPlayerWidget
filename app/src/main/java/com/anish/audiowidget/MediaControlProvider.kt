c2b3ef021a7f514c28df692c89130cf7a7bc0c27
Anish Sahoo <anishsahoo2005@gmail.com>
2023-11-02 20:55:44 -0700

    ui for widget almost done

--- app/src/main/java/com/anish/audiowidget/MediaControlProvider.kt
+++ app/src/main/java/com/anish/audiowidget/MediaControlProvider.kt
@@ -1,15 +1,18 @@
 package com.anish.audiowidget
 
 import android.appwidget.AppWidgetProvider
+import android.content.res.Configuration
 import android.widget.MediaController
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
@@ -17,6 +20,7 @@ import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,6 +29,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.anish.audiowidget.ui.theme.AudioWidgetTheme
@@ -39,7 +44,16 @@ class MediaControlProvider : AppWidgetProvider() {
     }
 }
 
-@Preview(showBackground = false)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    showBackground = false,
+    name = "Light Mode"
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = false,
+    name = "Dark Mode"
+)
 @Composable
 fun Preview() {
     AudioWidgetTheme {
@@ -55,51 +69,62 @@ fun MediaPlayerWidgetContainer(mediaController: MediaController?) {
 
 @Composable
 fun MediaPlayerWidget(mediaController: MediaController?) {
-
-    Column(Modifier.padding(8.dp)) {
-        // Display the title of the current track.
-        Row{
-            Box(
-                modifier = Modifier.size(50.dp, 50.dp).padding(end = 4.dp).clip(RoundedCornerShape(10.dp)).background(Color.White)
-            )
-            Column(Modifier.padding(0.dp, 0.dp, 2.dp, 0.dp)){
-                Text(
-                    text = "Song Name",
-                    color = Color.White,
-                    fontFamily = androidx.compose.ui.text.font.FontFamily.SansSerif
+    Box(
+        Modifier.background(MaterialTheme.colorScheme.primaryContainer)
+    ) {
+        Column(
+            Modifier
+                .padding(8.dp)
+        ) {
+            // displays album image and song name, artist name
+            Row {
+                Box(
+                    modifier = Modifier
+                        .size(50.dp, 50.dp)
+                        .padding(4.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Color.White)
                 )
-                Text(
-                    text = "Artist",
-                    color = Color.White,
-                    fontSize = 10.sp,
-                    fontFamily = androidx.compose.ui.text.font.FontFamily.SansSerif
-                )
+                Column(Modifier.padding(6.dp, 6.dp, 2.dp, 0.dp)) {
+                    Text(
+                        text = "Song Name",
+                        color = MaterialTheme.colorScheme.inverseSurface,
+                        fontFamily = androidx.compose.ui.text.font.FontFamily.SansSerif
+                    )
+                    Text(
+                        text = "Artist",
+                        color = MaterialTheme.colorScheme.inverseSurface,
+                        fontSize = 10.sp,
+                        fontFamily = androidx.compose.ui.text.font.FontFamily.SansSerif
+                    )
+                }
             }
-        }
+            // displays the media controller buttons
+            Row(Modifier.padding(0.dp, 2.dp, 2.dp, 0.dp)) {
+                // Display the previous track button.
 
-
-        Row(Modifier.padding(0.dp, 2.dp, 2.dp, 0.dp)){
-            // Display the previous track button.
-
-            Button(
-                onClick = {},
-                Modifier.padding(end = 2.dp)
-            ) {
-                Icon(Icons.Filled.KeyboardArrowLeft, "")
-            }
-            // Display the play/pause button.
-            Button(
-                onClick = {}
-            ) {
-                Icon(Icons.Filled.PlayArrow, contentDescription = null)
-            }
-            // Display the next track button.
-            Button(
-                onClick = {},
-                Modifier.padding(start = 2.dp)
-            ) {
-                Icon(Icons.Filled.KeyboardArrowRight, contentDescription = null)
+                Button(
+                    onClick = {},
+                    Modifier.padding(end = 2.dp),
+                ) {
+                    Icon(Icons.Filled.KeyboardArrowLeft, "")
+                }
+                // Display the play/pause button.
+                Button(
+                    onClick = {},
+                    shape = CircleShape
+                ) {
+                    Icon(Icons.Filled.PlayArrow, contentDescription = null)
+                }
+                // Display the next track button.
+                Button(
+                    onClick = {},
+                    Modifier.padding(start = 2.dp)
+                ) {
+                    Icon(Icons.Filled.KeyboardArrowRight, contentDescription = null)
+                }
             }
         }
     }
 }
+
